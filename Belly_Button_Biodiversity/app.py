a2d23619955f9ecf37c354ef65e3f2b829b540d1
Jonathan Piech <jonathan.piech@gmail.com>
2020-02-25 19:46:47 -0600

    comments

--- Belly_Button_Biodiversity/app.py
+++ Belly_Button_Biodiversity/app.py
@@ -13,17 +13,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-
-#################################################
 # Database Setup
-#################################################
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
 db = SQLAlchemy(app)
 
-# reflect an existing database into a new model
+# Reflect an existing database into a new model
 Base = automap_base()
-# reflect the tables
+
+# Reflect tables
 Base.prepare(db.engine, reflect=True)
 
 # Save references to each table
